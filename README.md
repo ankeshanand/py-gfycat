806d1eae71750bc4aeff2ae0c19438c32cd67f91
Ankesh Anand <ankeshanand1994@gmail.com>
2015-07-30 19:15:54 +0530

    Set up basic project structure, add constants, other minor fixes.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # py-gfycat
-A Python client for the GFYCAT API. It can be used to interact with the GFYCAT API in your projects.
+A Python client for the gfycat API. It can be used to interact with the gfycat API in your projects.
---
+++ gfycat/constants.py
@@ -0,0 +1,30 @@
+# Gfycat does not yet have a full fledged API with
+# access credentials and advanced access control.
+# These parameters are explicitly mentioned at
+# http://gfycat.com/api
+
+# Fetching from a URL
+FETCH_URL = 'http://upload.gfycat.com/transcode?fetchUrl='
+
+# File Upload
+FILE_UPLOAD_URL = 'https://gifaffe.s3.amazonaws.com/'
+FILE_UPLOAD_STATUS_URL = 'https://upload.gfycat.com/transcode/'
+ACL = 'private'
+AWS_ACCESS_KEY_ID = 'AKIAIT4VU4B7G2LQYKZQ'
+POLICY = ("eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICAgIC"
+          "AgICAgICAiY29uZGl0aW9ucyI6IFsKICAgICAgICAgICAgeyJidWNrZXQiOiAiZ2lm"
+          "YWZmZSJ9LAogICAgICAgICAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCi"
+          "AgICAgICAgICAgIHsiYWNsIjogInByaXZhdGUifSwKCSAgICB7InN1Y2Nlc3NfYWN0"
+          "aW9uX3N0YXR1cyI6ICIyMDAifSwKICAgICAgICAgICAgWyJzdGFydHMtd2l0aCIsIC"
+          "IkQ29udGVudC1UeXBlIiwgIiJdLAogICAgICAgICAgICBbImNvbnRlbnQtbGVuZ3Ro"
+          "LXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogICAgICAgICAgICBdCiAgICAgICAgICB9")
+SUCCESS_ACTION_STATUS = 200
+SIGNATURE = 'mk9t/U/wRN4/uU01mXfeTe2Kcoc='
+CONTENT_TYPE = 'image/gif'
+
+# Query
+QUERY_URL = 'http://gfycat.com/cajax/get/'
+
+# Check Link
+CHECK_LINK_URL = 'http://gfycat.com/cajax/checkUrl/'
+
