4f1711b24e76d789e2f79b99cebd027afd811128
Ankesh Anand <ankeshanand1994@gmail.com>
2015-07-30 23:02:17 +0530

    Add error and client functions.

--- gfycat/client.py
+++ gfycat/client.py
@@ -1,1 +1,79 @@
+import requests
+import uuid
+
+from constants import (FETCH_URL_ENDPOINT, FETCH_URL_LAZY_ENDPOINT,
+    FETCH_URL_STATUS_ENDPOINT, FILE_UPLOAD_ENDPOINT,
+    FILE_UPLOAD_STATUS_ENDPOINT, ACL, AWS_ACCESS_KEY_ID, POLICY,
+    SUCCESS_ACTION_STATUS, SIGNATURE, CONTENT_TYPE, QUERY_ENDPOINT,
+    CHECK_LINK_ENDPOINT)
+from error import GfycatClientError
+
+
+class GfycatClient(object):
+    def __init__(self):
+        # Will hold access tokens and auth credentials when Gfycat decides to
+        # implement them.
+        pass
+
+    def fetch_url(self, url):
+        params = {'fetchUrl': url}
+        r = requests.get(FETCH_URL_ENDPOINT, params=params)
+
+        if r.status_code != 200:
+            raise GfycatClientError('Error fetching the URL', r.status_code)
+
+        response = r.json()
+        return response
+
+    def fetch_url_lazy(self, url, random_string=None):
+        if not random_string:
+            random_string = str(uuid.uuid4())[:8]
+
+        params = {'fetchUrl': url}
+        r = requests.get(FETCH_URL_LAZY_ENDPOINT + random_string,
+                         params=params)
+
+        if r.status_code != 200:
+            raise GfycatClientError('Error fetching the URL', r.status_code)
+
+        return random_string
+
+    def check_fetch_status(self, random_string):
+        pass
+
+    def upload_file(self, filename, key=None):
+        """
+        Upload a local file to Gfycat
+        :param filename:
+        :param key: A unique string to later check the status of file upload
+        :return:
+        """
+        if not key:
+            key = str(uuid.uuid4())[:8]
+
+        form = [('key', key),
+                ('acl', ACL),
+                ('AWSAccessKeyId', AWS_ACCESS_KEY_ID),
+                ('success_action_status', SUCCESS_ACTION_STATUS),
+                ('signature', SIGNATURE),
+                ('Content-Type', CONTENT_TYPE),
+                ('policy', POLICY)]
+
+        with open(filename, 'rb') as upfile:
+            form.append(('file', upfile))
+            r = requests.post(FILE_UPLOAD_ENDPOINT, files=form)
+
+        if r.status_code != 200:
+            raise GfycatClientError('Error uploading the GIF', r.status_code)
+
+        return key
+
+    def check_upload_status(self, key):
+        pass
+
+    def query_gfy(self, gfyname):
+        pass
+
+    def check_link(self, link):
+        pass
 
--- gfycat/constants.py
+++ gfycat/constants.py
@@ -4,27 +4,22 @@
 # http://gfycat.com/api
 
 # Fetching from a URL
-FETCH_URL = 'http://upload.gfycat.com/transcode?fetchUrl='
+FETCH_URL_ENDPOINT = 'http://upload.gfycat.com/transcode'
+FETCH_URL_LAZY_ENDPOINT = 'http://upload.gfycat.com/transcodeRelease/'
+FETCH_URL_STATUS_ENDPOINT = 'https://upload.gfycat.com/status/'
 
 # File Upload
-FILE_UPLOAD_URL = 'https://gifaffe.s3.amazonaws.com/'
-FILE_UPLOAD_STATUS_URL = 'https://upload.gfycat.com/transcode/'
+FILE_UPLOAD_ENDPOINT = 'https://gifaffe.s3.amazonaws.com/'
+FILE_UPLOAD_STATUS_ENDPOINT = 'https://upload.gfycat.com/transcode/'
 ACL = 'private'
 AWS_ACCESS_KEY_ID = 'AKIAIT4VU4B7G2LQYKZQ'
-POLICY = ("eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICAgIC"
-          "AgICAgICAiY29uZGl0aW9ucyI6IFsKICAgICAgICAgICAgeyJidWNrZXQiOiAiZ2lm"
-          "YWZmZSJ9LAogICAgICAgICAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCi"
-          "AgICAgICAgICAgIHsiYWNsIjogInByaXZhdGUifSwKCSAgICB7InN1Y2Nlc3NfYWN0"
-          "aW9uX3N0YXR1cyI6ICIyMDAifSwKICAgICAgICAgICAgWyJzdGFydHMtd2l0aCIsIC"
-          "IkQ29udGVudC1UeXBlIiwgIiJdLAogICAgICAgICAgICBbImNvbnRlbnQtbGVuZ3Ro"
-          "LXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogICAgICAgICAgICBdCiAgICAgICAgICB9")
+POLICY = "eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICAgICAgICAgICAiY29uZGl0aW9ucyI6IFsKICAgICAgICAgICAgeyJidWNrZXQiOiAiZ2lmYWZmZSJ9LAogICAgICAgICAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCiAgICAgICAgICAgIHsiYWNsIjogInByaXZhdGUifSwKCSAgICB7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6ICIyMDAifSwKICAgICAgICAgICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgICAgICAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogICAgICAgICAgICBdCiAgICAgICAgICB9"
 SUCCESS_ACTION_STATUS = 200
 SIGNATURE = 'mk9t/U/wRN4/uU01mXfeTe2Kcoc='
 CONTENT_TYPE = 'image/gif'
 
 # Query
-QUERY_URL = 'http://gfycat.com/cajax/get/'
+QUERY_ENDPOINT = 'http://gfycat.com/cajax/get/'
 
 # Check Link
-CHECK_LINK_URL = 'http://gfycat.com/cajax/checkUrl/'
-
+CHECK_LINK_ENDPOINT = 'http://gfycat.com/cajax/checkUrl/'
